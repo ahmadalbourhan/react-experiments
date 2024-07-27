5878f126c9b4bcfaf7e8c1944418f714e6cbed86
ahmadalbourhan <127745237+ahmadalbourhan@users.noreply.github.com>
2024-07-27 20:45:40 +0300

    useState hook

---
+++ useState-hook/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+interface DemoProps {}
+
+export default function Demo({}: DemoProps) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>
+        Decrement
+      </button>
+      <button onClick={() => setCount(0)}>
+        Clear
+      </button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
