e6f1ef55b39687e42c6411173e9844548ab8b343
ahmadalbourhan <127745237+ahmadalbourhan@users.noreply.github.com>
2024-07-29 17:53:27 +0300

    updated files

--- useEffect-hook/src/App.tsx
+++ useEffect-hook/src/App.tsx
@@ -1,31 +1,24 @@
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     // The code that we want to run
-    console.log('The count is:', count);
+    console.log("The count is:", count);
 
     // Optional return function
     return () => {
-      console.log('I am being cleaned up!');
+      console.log("I am being cleaned up!");
     };
   }, [count]); // The dependency array
 
   return (
-    <div className='tutorial'>
+    <div className="tutorial">
       <h1>Count: {count}</h1>
-      <button onClick={() => setCount(prev => prev - 1)}>
-        Decrement
-      </button>
-       <button onClick={() => setCount(0)}>
-        Clear
-      </button>
-      <button onClick={() => setCount(prev => prev + 1)}>
-        Increment
-      </button>
+      <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Clear</button>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
     </div>
   );
 }
